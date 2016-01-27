823519969c31efd497874ffef126d67330fc2221
mz2 <matias.piipari@gmail.com>
2016-01-27 12:54:39 +0000

    Adds a KissXML.xcodeproj, a framework target, and a shared scheme to build it.

---
+++ KissXML/KissXML/KissXML/KissXML.h
@@ -0,0 +1,24 @@
+//
+//  KissXML.h
+//  KissXML
+//
+//  Created by Matias Piipari on 27/01/2016.
+//  Copyright © 2016 Mekentosj BV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KissXML.
+FOUNDATION_EXPORT double KissXMLVersionNumber;
+
+//! Project version string for KissXML.
+FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KissXML/PublicHeader.h>
+
+#import "DDXML.h"
+#import "DDXMLDocument.h"
+#import "DDXMLElement.h"
+#import "DDXMLNode.h"
+#import "DDXMLElementAdditions.h"
+#import "NSString+DDXML.h"
