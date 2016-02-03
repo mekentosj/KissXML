8038aa684e84a6169fe261bff519de76635d69d0
mz2 <matias.piipari@gmail.com>
2016-02-03 12:15:25 -0500

    Moves libxml/tree.h import into NSString+DDXML implementation.

--- KissXML/Categories/NSString+DDXML.h
+++ KissXML/Categories/NSString+DDXML.h
@@ -1,13 +1,11 @@
 #import <Foundation/Foundation.h>
-#import <libxml/tree.h>
-
 
 @interface NSString (DDXML)
 
 /**
  * xmlChar - A basic replacement for char, a byte in a UTF-8 encoded string.
 **/
-- (const xmlChar *)xmlChar;
+- (const unsigned char *)xmlChar;
 
 - (NSString *)stringByTrimming;
 
--- KissXML/Categories/NSString+DDXML.m
+++ KissXML/Categories/NSString+DDXML.m
@@ -1,4 +1,5 @@
 #import "NSString+DDXML.h"
+#import <libxml/tree.h>
 
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
